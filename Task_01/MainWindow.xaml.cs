81f8831a719e8ef3588a03a6bf748bafdfba7090
bmvolf <bmvolgin@gmail.com>
2023-01-16 20:36:54 +0300

    Task 1

--- Task_01/MainWindow.xaml.cs
+++ Task_01/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Task_01
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You pushed this button");
+        }
     }
 }
